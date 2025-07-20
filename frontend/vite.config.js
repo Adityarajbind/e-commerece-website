e62a3001904418a58b8bd450bf1e41c2812eee45
Adityaraj Bind <126939191+Adityarajbind@users.noreply.github.com>
2025-07-20 20:33:35 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [tailwindcss(),react()],
   server: {
   proxy: {
-    '/api': 'http://localhost:5000'
+    '/api': 'https://e-commerece-website-backend.onrender.com'
   }
 }
 })
